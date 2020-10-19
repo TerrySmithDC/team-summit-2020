aab85df4cc85e105d31c9461ad6cb4ed35d6bed2
Terry Smith <terry@codecov.io>
2020-10-19 23:26:57 +0000

    Lazy load all of react 3 fiber

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-
-import Canvas from "./Canvas";
+import React, { Suspense, lazy} from "react";
 import "./App.css";
+
+const Canvas = lazy(() => import('./Canvas'));
 
 function App() {
   return (
     <div className="App">
-      <Canvas />
+      <Suspense fallback={null}>
+        <Canvas />
+      </Suspense>
       <article>Codecov Team Summit 2020</article>
     </div>
   );
